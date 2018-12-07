c2f6218b20b1c6b980c1841b7ff1dd450c02024f
Rafatlne <ashikrafat.lne@gmail.com>
2018-12-07 16:46:59 +0600

    bug fix

--- api/room/update.php
+++ api/room/update.php
@@ -35,7 +35,7 @@ if($roomQuery->update()){
     http_response_code(200);
  
     // tell the user
-    echo json_encode(array("successful"));
+    echo json_encode(array("message" => "successful"));
 }
  
 // if unable to update the product, tell the user
